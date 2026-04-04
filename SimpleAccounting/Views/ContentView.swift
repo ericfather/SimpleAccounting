9a700504edba7ae7ace425284611bc0d37894a0b
ericfather <daianding131@gmail.com>
2026-04-04 19:30:02 +0800

    Update ContentView.swift

--- SimpleAccounting/Views/ContentView.swift
+++ SimpleAccounting/Views/ContentView.swift
@@ -7,8 +7,6 @@ struct ContentView: View {
     @State private var dataService = DataService.shared
 
     var body: some View {
-        // 临时测试用
-let _ = (0..<10000).map { "\($0)" }
         TabView(selection: $selectedTab) {
             HomeView()
                 .tabItem {
